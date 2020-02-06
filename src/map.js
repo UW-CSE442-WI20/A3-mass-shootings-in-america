233129e055e1005a7e0ed46d50ff9240d2bdf487
Rebecca Bol <rbol@cs.washington.edu>
2020-02-05 18:39:32 -0800

    Updated dot sizes

--- src/map.js
+++ src/map.js
@@ -1,11 +1,6 @@
-var data = require("./us-states.json");
+var topology = require("./us-states.json");
 var width = 975;
 var height = 610;
-
-var projection = d3
-  .geoAlbersUsa()
-  .scale(1300)
-  .translate([487.5, 305]);
 
 var path = d3.geoPath();
 
@@ -15,14 +10,61 @@ var map = d3
   .append("svg")
   .attr("width", width)
   .attr("height", height)
-  .style("fill", "none")
-  .style("stroke", "steelblue");
+  .attr("id", "map");
 
 // add usa border
 map
   .append("path")
-  .attr("d", path(topojson.feature(data, data.objects.nation)))
-  .style("stroke", "black");
+  .attr("class", "map")
+  .attr("d", path(topojson.feature(topology, topology.objects.nation)));
 
 // add state borders
-map.append("path").attr("d", path(topojson.feature(data, data.objects.states)));
+map
+  .append("path")
+  .attr("class", "map")
+  .attr("d", path(topojson.feature(topology, topology.objects.states)));
+
+var tooltip = d3
+  .select("map")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+var projection = d3
+  .geoAlbersUsa()
+  .scale(1300)
+  .translate([487.5, 305]);
+
+// add data points
+var coordinates = [];
+var data = require("./data.csv");
+d3.csv(data, function(row) {
+  let coord = [row.longitude, row.latitude];
+  let x = projection(coord)[0],
+    y = projection(coord)[1];
+  map
+    .append("circle")
+    .attr("cx", x)
+    .attr("cy", y)
+    .attr("r", Math.sqrt(row.total_victims))
+    .attr("fill", "red")
+    .attr("opacity", 1)
+    .on("mouseover", function() {
+      d3.select(this).attr("opacity", 0.8);
+      tooltip
+        .text(row.case)
+        .attr("opacity", 0.6)
+        .attr("x", d3.event.pageX)
+        .attr("y", d3.event.pageY)
+        .transition()
+        .duration(200);
+    })
+    .on("mouseout", function() {
+      d3.select(this).attr("opacity", 1);
+      tooltip
+        .transition()
+        .duration(500)
+        .attr("opacity", 0);
+    });
+  coordinates.push([x, y]);
+});
