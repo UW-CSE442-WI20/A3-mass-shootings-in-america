d0bdf34f6e8f4bafaa2f8fb15f8687a6a13fbc1e
Andrew Siew <aks42@cs.washington.edu>
2020-02-05 17:43:16 -0800

    dots appear now

--- src/map.js
+++ src/map.js
@@ -1,11 +1,19 @@
 var data = require("./us-states.json");
+var shooting_data = require("./data.json");
+
+// Parsing data 
+var coordinates = [];
+shooting_data.forEach(function (shooting) {
+  coordinates.push([shooting.longitude, shooting.latitude]);
+});
+
 var width = 975;
 var height = 610;
 
 var projection = d3
   .geoAlbersUsa()
   .scale(1300)
-  .translate([487.5, 305]);
+  .translate([width / 2, height / 2]);
 
 var path = d3.geoPath();
 
@@ -26,3 +34,21 @@ map
 
 // add state borders
 map.append("path").attr("d", path(topojson.feature(data, data.objects.states)));
+
+// Adding points
+
+aa = [-122.490402, 37.786453];
+	bb = [-122.389809, 37.72728];
+
+map.selectAll("circle")
+		.data(coordinates).enter()
+		.append("circle")
+		.attr("cx", function (d) { 
+      return projection(d)[0]; 
+    })
+		.attr("cy", function (d) { 
+      return projection(d)[1]; 
+    })
+		.attr("r", "8px")
+    .attr("fill", "red")
+    
