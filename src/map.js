30f5fb418025d9a14e0e0aa320f7f566201755d4
Rebecca Bol <rbol@cs.washington.edu>
2020-02-06 13:04:10 -0800

    Added zoom in feature for clicking on point

--- src/map.js
+++ src/map.js
@@ -1,15 +1,16 @@
 import "regenerator-runtime/runtime";
 var topology = require("./us-states.json");
-var width = 975;
-var height = 610;
+var width = 975,
+  height = 610,
+  centered;
 
+var defaultView = "0 0 " + width + " " + height;
 var projection = d3
   .geoAlbersUsa()
   .scale(1300)
   .translate([487.5, 305]);
 
 var path = d3.geoPath();
-var coordinates = [];
 var data = require("./data.csv");
 var smallest_year = 9999;
 var largest_year = 0;
@@ -18,53 +19,53 @@ var allData = [];
 var curData = [];
 var year_to_data = {};
 
-async function parseData(){
+async function parseData() {
   await d3.csv(data, async function(row) {
     allData.push(row);
-    if(year_to_data[row.year] === undefined){
+    if (year_to_data[row.year] === undefined) {
       year_to_data[row.year] = [row];
-    }else{
+    } else {
       year_to_data[row.year].push(row);
     }
     smallest_year = Math.min(smallest_year, parseInt(row.year));
     largest_year = Math.max(largest_year, parseInt(row.year));
-  })
+  });
 }
 
-async function renderMap(){
+var zoomed = false;
+async function renderMap() {
   // Initialize svg object
   var map = d3
-  .select("#map")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("id", "map")
-  .attr("class", "tooltip");
+    .select("#map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("id", "map")
+    .attr("viewBox", defaultView)
+    .attr("preserveAspectRatio", "xMidYMid meet");
 
   // Adding usa border
   map
-  .append("path")
-  .attr("class", "map")
-  .attr("d", path(topojson.feature(topology, topology.objects.nation)));
+    .append("path")
+    .attr("class", "map")
+    .attr("d", path(topojson.feature(topology, topology.objects.nation)));
 
   // Adding state borders
   map
-  .append("path")
-  .attr("class", "map")
-  .attr("d", path(topojson.feature(topology, topology.objects.states)));
+    .append("path")
+    .attr("class", "map")
+    .attr("d", path(topojson.feature(topology, topology.objects.states)));
 
-  var infoWindowOpen;
   var tooltip = d3
-  .select("body")
-  .append("div")
-  .attr("class", "tooltip")
-  .on("mouseon", (infoWindowOpen = true));
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip");
 
-  for(let i = 0; i < curData.length; i++){
+  for (let i = 0; i < curData.length; i++) {
     let row = curData[i];
     let coord = [row.longitude, row.latitude];
     let x = projection(coord)[0],
-        y = projection(coord)[1];
+      y = projection(coord)[1];
 
     map
       .append("circle")
@@ -83,59 +84,56 @@ async function renderMap(){
       })
       .on("mouseout", function() {
         d3.select(this).attr("opacity", 1);
-        if (!infoWindowOpen) {
-          tooltip.html("").style("opacity", 0);
-        }
+        tooltip.html("").style("opacity", 0);
       })
-    .on("click", function() {
-      d3.select(this).attr("opacity", 0.6);
-      tooltip.transition().ease();
-      tooltip.html(
-        "<b>" +
-          row.case +
-          "</b><br>(" +
-          row.location +
-          ")" +
-          "<br><br>Fatalities: " +
-          row.fatalities +
-          "<br>Injuries: " +
-          row.injured +
-          "<br><br><a href=" +
-          row.sources +
-          ">More info</a>"
-      );
-    });
-  };
+      .on("click", function() {
+        if (zoomed) {
+          zoomed = false;
+          map
+            .transition()
+            .duration(300)
+            .attr("viewBox", defaultView);
+        } else {
+          zoomed = true;
+          let center = "" + d3.event.pageX / 2 + " " + d3.event.pageY / 2 + " ";
+          map
+            .transition()
+            .duration(300)
+            .attr("viewBox", center + " " + center);
+        }
+      });
+  }
 }
 
-async function initSlider(){
+async function initSlider() {
   var slider_label = document.getElementById("slider_year");
   var slider = document.getElementById("slider");
   var all_years = document.getElementById("all_years_button");
 
-  all_years.addEventListener("click", function(){
+  all_years.addEventListener("click", function() {
     slider_label.innerText = "All years";
     curData = allData;
     document.getElementById("map").innerHTML = "";
     renderMap();
-  })
+  });
   slider_label.innerText = "All years";
   slider.setAttribute("max", largest_year);
   slider.setAttribute("min", smallest_year);
-  slider.addEventListener("change", function(){
-    document.getElementById("slider_year").innerText = document.getElementById("slider").value;
+  slider.addEventListener("change", function() {
+    document.getElementById("slider_year").innerText = document.getElementById(
+      "slider"
+    ).value;
     let temp = year_to_data[document.getElementById("slider").value];
-    if(temp === undefined){
+    if (temp === undefined) {
       curData = [];
     }
     curData = temp;
     document.getElementById("map").innerHTML = "";
     renderMap();
   });
-
 }
 
-async function init(){
+async function init() {
   await parseData();
   curData = allData;
   await renderMap();
@@ -145,3 +143,22 @@ async function init(){
 init();
 // Manage slider
 
+function openInfoWindow(row) {
+  infoWindow.transition().ease();
+  infoWindow
+    .toggle()
+    .html(
+      "<b>" +
+        row.case +
+        "</b><br>(" +
+        row.location +
+        ")" +
+        "<br><br>Fatalities: " +
+        row.fatalities +
+        "<br>Injuries: " +
+        row.injured +
+        "<br><br><a href=" +
+        row.sources +
+        ">More info</a>"
+    );
+}
