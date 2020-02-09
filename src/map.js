a516b73347a96b590bd0bb1e989454c36076bde9
Rebecca Bol <rbol@cs.washington.edu>
2020-02-08 16:32:08 -0800

    Pan zooming feature added

--- src/map.js
+++ src/map.js
@@ -1,19 +1,7 @@
 import "regenerator-runtime/runtime";
 
-var filters = {
-  fatalities: {
-    img: "https://img.icons8.com/ios-glyphs/30/000000/self-destruct-button.png",
-    col: 1
-  },
-  weapon: { img: "https://img.icons8.com/metro/26/000000/gun.png", col: 2 },
-  age: { img: "https://img.icons8.com/metro/26/000000/age.png", col: 3 },
-  gender: {
-    img: "https://img.icons8.com/metro/26/000000/gender--v1.png",
-    col: 4
-  }
-};
 var topology = require("./us-states.json");
-var map, tooltip, info, legend;
+var map, tooltip, info, view, zoom;
 var width = 975,
   height = 610,
   focus = d3.select(null);
@@ -21,11 +9,11 @@ var defaultView = viewToString(0, 0, width, height);
 
 var currentFilter;
 var projection = d3
-  .geoAlbersUsa()
-  .scale(1300)
-  .translate([487.5, 305]);
+    .geoAlbersUsa()
+    .scale(1300)
+    .translate([487.5, 305]),
+  path = d3.geoPath();
 
-var path = d3.geoPath();
 var data = require("./data.csv");
 var smallest_year = 9999;
 var largest_year = 0;
@@ -58,7 +46,7 @@ async function renderMap() {
     .attr("viewBox", defaultView)
     .attr("preserveAspectRatio", "xMidYMid slice")
     .attr("id", "map")
-    .on("click", handleClick);
+    .append("g");
 
   tooltip = d3
     .select("#map")
@@ -70,6 +58,19 @@ async function renderMap() {
     .append("div")
     .attr("class", "info")
     .attr("height", "0px");
+
+  zoom = d3
+    .zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
+  view = map
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .style("fill", "none")
+    .style("pointer-events", "all");
+  map.call(zoom);
 
   // Adding usa border
   map
@@ -90,7 +91,7 @@ async function renderMap() {
       y = projection(coord)[1];
 
     // Adding circles
-    var p = map
+    map
       .append("circle")
       .attr("cx", x)
       .attr("cy", y)
@@ -116,63 +117,44 @@ async function renderMap() {
 }
 
 function handleClick() {
-  if (focus.node() === this || this["id"] !== "point") {
-    focus = d3.select(null);
-    // reset to default view
-    info
-      .html("")
-      .style("padding", 0)
-      .transition()
-      .duration(300)
-      .style("height", 0 + "px");
-    zoom(defaultView);
-  } else {
-    focus = d3.select(this);
-    event.stopPropagation();
-    let x = Math.round(focus._groups[0][0]["cx"].baseVal.value),
-      y = Math.round(focus._groups[0][0]["cy"].baseVal.value),
-      size = 500;
-
-    handleWindow(x, y, coord_to_data[(x, y)]);
-    let view = viewToString(x - size / 2, y - size / 2, size, size);
-    zoom(view);
+  if (this["id"] === "point") {
+    let p = d3.select(this);
+    let x = Math.round(p._groups[0][0]["cx"].baseVal.value),
+      y = Math.round(p._groups[0][0]["cy"].baseVal.value);
+    openPanel(coord_to_data[(x, y)]);
   }
+}
 
-  function zoom(view) {
-    map
-      .transition()
-      .duration(750)
-      .attr("viewBox", view);
-  }
+function zoomed() {
+  console.log("here");
+  map.attr("transform", d3.event.transform);
+}
 
-  function handleWindow(x, y, pointData) {
-    var scale = 1.3;
-    x =
-      x < width / 2
-        ? Math.max(x - x * scale, 50)
-        : Math.min(x + x * scale, 700);
-    y =
-      y < height / 2 ? Math.max(y - y * scale, 150) : Math.min(y * scale, 250);
+function openPanel(pointData) {
+  var externalLink = pointData.sources.split(";")[0];
+  var w = 250,
+    h = height;
+  var embedd =
+    "<iframe sandbox=allow-scripts width=" +
+    w +
+    " height= " +
+    h +
+    " src=" +
+    externalLink +
+    "</iframe>";
 
-    var externalLink = pointData.sources.split(";")[0];
-    var embedd =
-      "<iframe sandbox=allow-scripts width=400 height=500 src=" +
-      externalLink +
-      "</iframe>";
-
-    info
-      .html(embedd)
-      .style("left", x + "px")
-      .style("top", y + "px")
-      .style("height", 0 + "px")
-      .style("opacity", 0)
-      .style("padding", "5px 5px 5px 5px")
-      .transition()
-      .duration(1000)
-      .style("opacity", 0.9)
-      .style("width", 400 + "px")
-      .style("height", 500 + "px");
-  }
+  info
+    .html(embedd)
+    .style("right", 0 + "%")
+    .style("top", 80 + "px")
+    .style("height", 0 + "px")
+    .style("opacity", 0)
+    .style("padding", "5px 5px 5px 5px")
+    .transition()
+    .duration(1000)
+    .style("opacity", 0.9)
+    .style("width", w + "px")
+    .style("height", h + "px");
 }
 
 function viewToString(x1, y1, x2, y2) {
@@ -207,47 +189,9 @@ async function initSlider() {
   });
 }
 
-async function initFilter() {
-  d3.select("filter").attr("opacity", 0.2);
-  d3.select("filter")
-    .text("Filters")
-    .attr("class", "desc");
-  legend = d3.select("filter").append("div");
-  let menu = d3
-    .select("filter")
-    .append("div")
-    .attr("class", "menu");
-
-  Object.keys(filters).forEach(function(key) {
-    menu
-      .append("img")
-      .attr("src", filters[key].img)
-      .attr("id", key)
-      .on("click", updateFilter);
-  });
-
-  menu
-    .append("svg")
-    .attr("id", "selector")
-    .attr("class", "selector")
-    .style("grid-column", 4);
-}
-
-function updateFilter() {
-  console.log("here");
-  currentFilter = d3.select(this);
-
-  var move = filters[currentFilter.node()["id"]].col;
-  d3.select("selector")
-    .transition()
-    .duration(400)
-    .style("grid-column", move);
-}
-
 async function init() {
   await parseData();
   curData = allData;
-  await initFilter();
   await renderMap();
   await initSlider();
 }
