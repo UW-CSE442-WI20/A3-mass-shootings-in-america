3576a62c30c5ff2c0312f31e124d7f0bf6348d01
samuel <mnkloip@uw.edu>
2020-02-12 14:06:17 -0800

    hope

--- docs/map.27237bf4.js
+++ docs/map.27237bf4.js
@@ -1937,7 +1937,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51338" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50309" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
